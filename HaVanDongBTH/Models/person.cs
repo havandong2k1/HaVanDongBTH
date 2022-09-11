071a2cf98a1d823112954854682a997f2b0346c0
havandong2k1 <111983151+havandong2k1@users.noreply.github.com>
2022-09-11 21:23:46 +0700

    HaVanDong_0709

---
+++ HaVanDongBTH/Models/person.cs
@@ -0,0 +1,9 @@
+namespace HaVanDongBTH.Models
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set;}
+    }
+}
