6a44887275c6648a8b1f1c4e8709dbaa1f4db88a
havandong2k1 <111983151+havandong2k1@users.noreply.github.com>
2023-03-13 10:42:16 +0700

    baithuchanh0703

--- baithuchanh0703/Controllers/StudentController.cs
+++ baithuchanh0703/Controllers/StudentController.cs
@@ -16,7 +16,40 @@ namespace baitapthuchanh0703.Controllers
             ViewBag.abc = strReturn;
             return View();
         }
-      
+        public IAsyncResult Bachai()
+    {
+        return View();
+           
+    }
+        [HttpPost]
+        public IAsyncResult Bachai(string hesoA,string hesoB,string hesoC)
+    {
+            
+            double delta, x1, x2, a=0,b=0,c=0;
+            string ketqua;
+            if(!String.IsNullOrEmpty(hesoA)) a = Convert.ToDouble(hesoA);
+            if(!String.IsNullOrEmpty(hesoB)) a = Convert.ToDouble(hesoB);
+            if(!String.IsNullOrEmpty(hesoC)) a = Convert.ToDouble(hesoC);
+            if(a==0) ketqua = "Khong phai phuong trinh bac 2";
+            else
+            {
+                delta = Math.Pow(b,2) - 4 * a * c;
+                if(delta<0) ketqua ="Phuong trinh vo nghiem";
+                else if(delta ==0)
+                {
+                    x1 = -b/(2*a);
+                    ketqua ="Phuong trinh co nghiem kep  = " + x1;
+                }
+                else
+                {
+                    x1 = (-b +  Math.Sqrt(delta))/(2*a);
+                    x2 = (-b +  Math.Sqrt(delta))/(2*a);
+                    ketqua = "Phuong trinh co 2 hai nghiem phaan biet: X1= " + x1 +  ", X2 = " x2;
+                }
+            }
+            ViewBag.abc =ketqua;
+            return View();
+        }
     }
     
 }
